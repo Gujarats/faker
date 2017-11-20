7da5deb71fdbb95702503066da28e8b7f85c5236
gujarats <gujarat.santana@gmail.com>
2017-11-20 17:02:08 +0700

    generator for person's name

--- person.go
+++ person.go
@@ -4,35 +4,56 @@ import (
 	"github.com/Gujarats/faker/provider/en_US"
 )
 
-var gender = []string{"male", "female"}
+var genders = []string{"male", "female"}
 
 // Get a random name for specific male or female if specify
 func Name() string {
-	//genderType := getRandomIndex(gender)
-	return ""
+	PersonGenerator := []string{PersonSuffix(), Person()}
+	return PersonGenerator[getRandomIndex(PersonGenerator)]
+}
+
+// generate person with kind of rand suffix,male ,female
+func PersonSuffix() string {
+	gender := genders[getRandomIndex(genders)]
+	if gender == "male" {
+		return PersonMale()
+	} else {
+		return PersonFemale()
+	}
 }
 
 func Person() string {
-	// generate person with kind of rand prefix male and female and degree
-	_ = Prefix()
-
-	return ""
+	gender := genders[getRandomIndex(genders)]
+	if gender == "male" {
+		return Male()
+	} else {
+		return Female()
+	}
 }
 
-func Prefix() string {
-	return ""
-
+func PersonMale() string {
+	return Male() + " " + suffix()
 }
 
-func Degree() string {
-	return ""
+func PersonFemale() string {
+	return Female() + " " + suffix()
+}
 
+func suffix() string {
+	return en_US.Suffix[getRandomIndex(en_US.Suffix)]
 }
 
 // return random male name with first and last name
-func male() string {
+func Male() string {
 	firstNameMale := en_US.FirstNameMale[getRandomIndex(en_US.FirstNameMale)]
-	lastNameMale := en_US.LastName[getRandomIndex(en_US.LastName)]
+	return firstNameMale + " " + lastName()
+}
 
-	return firstNameMale + " " + lastNameMale
+func Female() string {
+	firstNameFemale := en_US.FirstNameFemale[getRandomIndex(en_US.FirstNameFemale)]
+	return firstNameFemale + " " + lastName()
+}
+
+func lastName() string {
+	return en_US.LastName[getRandomIndex(en_US.LastName)]
 }
--- person_test.go
+++ person_test.go
@@ -2,13 +2,15 @@ package faker
 
 import (
 	"testing"
+
+	"github.com/Gujarats/logger"
 )
 
 // get random 10 male's name
 func TestMale(t *testing.T) {
 	names := make(map[string]bool)
 	for i := 0; i < 10; i++ {
-		name := male()
+		name := Male()
 		if _, ok := names[name]; ok {
 			t.Errorf("the males() function return the duplicate value or previous generated value ")
 		} else {
@@ -16,4 +18,18 @@ func TestMale(t *testing.T) {
 		}
 	}
 
+}
+
+func TestName(t *testing.T) {
+	names := make(map[string]bool)
+	for i := 0; i < 10; i++ {
+		name := Name()
+		if _, ok := names[name]; ok {
+			t.Errorf("the males() function return the duplicate value or previous generated value ")
+		} else {
+			names[name] = true
+		}
+	}
+
+	logger.Debug("result = ", names)
 }
